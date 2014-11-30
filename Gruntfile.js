7de43e5a507831f071932ab5edfc9c9eb9cfc27c
Jan Marten <jan.marten@gmx.de>
2014-11-30 12:13:17 +0100

    Don't rev favicon

--- Gruntfile.js
+++ Gruntfile.js
@@ -246,7 +246,8 @@ module.exports = function(grunt) {
                         '<%= config.dist %>/styles/{,*/}*.css',
                         '<%= config.dist %>/images/{,*/}*.*',
                         '<%= config.dist %>/styles/fonts/{,*/}*.*',
-                        '<%= config.dist %>/*.{ico,png}'
+                        '<%= config.dist %>/*.{ico,png}',
+                        '!<%= config.dist %>/favicon.ico'
                     ]
                 }
             }
