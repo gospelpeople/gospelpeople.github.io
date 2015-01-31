9ecd95b43df14be6bfe4df7f70fbcf41a3f0b474
Jan Marten <jan.marten@gmx.de>
2015-01-31 14:22:58 +0100

    Don't rev history images

--- Gruntfile.js
+++ Gruntfile.js
@@ -288,7 +288,8 @@ module.exports = function(grunt) {
                         '<%= config.dist %>/images/**/*.*',
                         '<%= config.dist %>/styles/fonts/{,*/}*.*',
                         '<%= config.dist %>/*.{ico,png}',
-                        '!<%= config.dist %>/favicon.ico'
+                        '!<%= config.dist %>/favicon.ico',
+                        '!<%= config.dist %>/images/history/**/*.*'
                     ]
                 }
             }
