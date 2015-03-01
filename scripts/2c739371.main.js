0fb1196857d00ec6ebf53a61926144f20f3bb57e
Jan Marten <jan.marten@gmx.de>
2015-03-01 09:33:27 +0100

    Updates

--- scripts/2c739371.main.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(document).ready(function(){$("#timeline-embed").arrive(".vco-main",function(){$(".image-popup").magnificPopup({type:"image"}),$(document).unbindArrive("#timeline-embed")}),$("#timeline-embed").on("UPDATE",function(){$("#timeline-embed").arrive(".image-popup",function(){$(this).magnificPopup({type:"image"})}),$(document).unbindArrive("#timeline-embed")})});
