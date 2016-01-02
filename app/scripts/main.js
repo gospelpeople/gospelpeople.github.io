08e3bb1aa13537399e254a46a5d93edd8dc4362d
Jan Marten <jan.marten@gmx.de>
2016-01-02 16:07:05 +0100

    Use new TimelineJS3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,24 +6,4 @@ $(document).ready(function () {
     type: 'image'
   });
 
-  $('#timeline-embed').arrive('.slider-container', function () {
-    setTimeout(function () {
-      $('.image-popup').magnificPopup({
-        type: 'image'
-      });
-    }, 1000);
-    $(document).unbindArrive('#timeline-embed');
-  });
-  $('#timeline-embed').on('UPDATE', function () {
-    $('.image-popup').magnificPopup({
-      type: 'image'
-    });
-    $('#timeline-embed .slider-container').arrive('.image-popup', function () {
-      $(this).magnificPopup({
-        type: 'image'
-      });
-    });
-    $(document).unbindArrive('#timeline-embed');
-  });
-
 });
